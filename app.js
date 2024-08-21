f2dbfcd7690ea1baaea2824535e39444386b4b71
Caynã Victor <caynavictor7@gmail.com>
2024-08-21 08:39:03 -0300

    fazendo alterações

--- app.js
+++ app.js
@@ -57,11 +57,10 @@ function descriptar(stringDescriptada) {
 
 // Copiar
 
-function copyToClickBoard()
-{
-    document.getElementById(".container__mensagem__botao").addEventListener('click', clipboardCopy);
-    async function clipboardCopy() {
-    let text = document.querySelector(".container__mensagem__textarea").value;
-    await navigator.clipboard.writeText(text);
-}
+function copiar(){
+    navigator.clipboard
+    .readText()
+    .then(
+        (clipText) => (document.querySelector(".container__mensagem__textarea").innerText += clipText),
+    );
 }
